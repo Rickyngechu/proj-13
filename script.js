d353fb7fd205666f6a3f38bbb6e21d37761ff7b6
Rickyngechu <ekngechu003@gmail.com>
2023-11-07 01:00:29 +0300

    Updates

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+const toggle = document.querySelector(".toggle-checkbox");
+
+// theme var
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// Manual theme switch
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+};
+
+toggle.addEventListener("click", themeSwitch);
